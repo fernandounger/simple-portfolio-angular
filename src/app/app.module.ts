25c6dc3c39d09e73a10f7257ec8a05bc6f71e2e3
Fernando Unger <fdasilvaunger@gmail.com>
2022-10-14 10:50:30 -0300

    feat: adding the component portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PortfolioComponent } from './portfolio/component/portfolio/portfolio.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+
 
 @NgModule({
   declarations: [
